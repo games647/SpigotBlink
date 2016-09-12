53385e5a023a1bbf37ae924cb711c9e2d53b5f7e
games647 <games@tsprivat.de>
2016-09-12 15:10:22 +0200

    First commit

---
+++ ReadMe.md
@@ -0,0 +1,19 @@
+# SpigotBlink
+
+## Description
+
+This project connects to [SpigotMc](https://www.spigotmc.org) (Xenoforo-Forum). If there are new unread notifications, it connects
+to the [Blink1](https://blink1.thingm.com/) USB-device and starts blinking in orange like the forum color.
+
+This project was created because there were no [ifttt.com](https://ifttt.com/) recipes for this.
+
+## Setup
+
+1. Get python3
+2. Install dependencies
+`pip install blink1`
+3. Now you can run it with `python3 spigot_blink.py`
+
+## Image
+
+![usb-blink-orange](https://i.imgur.com/Djifgmu.jpg)
---
+++ spigot_blink.py
@@ -0,0 +1,75 @@
+#! /usr/bin/python3
+
+from blink1.blink1 import Blink1
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+import urllib
+import time
+import json
+import struct
+import configparser
+
+# constants
+USER_AGENT = 'SpigotBlink'
+SPIGOT_COLOR = "ed8106"
+SPIGOT_RGB = struct.unpack('BBB', bytes.fromhex(SPIGOT_COLOR))
+
+UNREAD_ALERTS_KEY = "_visitor_alertsUnread"
+UNREAD_CONVERSATION_KEY = "_visitor_conversationsUnread"
+
+FADE_TIME = 500
+# seconds
+UPDATE_INTERVAL = 60
+
+
+def search_updates(token, user_id):
+    post_fields = {
+        '_xfToken': token,
+        '_xfResponseType': 'json'
+    }
+
+    request = Request('https://www.spigotmc.org/index.php?liveupdate', urlencode(post_fields).encode())
+    request.add_header('Cookie', 'xf_user=' + urllib.parse.quote(user_id))
+
+    # otherwise cloudflare would kick us out
+    request.add_header('User-Agent', USER_AGENT)
+    data = json.loads(urlopen(request).read().decode())
+
+    print("UPDATE")
+
+    unread = int(data[UNREAD_ALERTS_KEY])
+    return unread > 0
+
+
+def main():
+    device = Blink1()
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    token = config['Settings']['TOKEN']
+    user_id = config['Settings']['USER_ID']
+    main_loop(device, token, user_id)
+
+
+def main_loop(device, token, user_id):
+    unread_alert = search_updates(token, user_id)
+    last_check = int(round(time.time() * 1000))
+    while True:
+        now = int(round(time.time() * 1000))
+        if last_check + 3 * 1000 <= now:
+            # update RSS FEED
+            unread_alert = search_updates(token, user_id)
+            last_check = now
+
+        if unread_alert:
+            device.fade_to_rgb(FADE_TIME, SPIGOT_RGB[0], SPIGOT_RGB[1], SPIGOT_RGB[2])
+
+            time.sleep(3)
+
+            # white
+            device.fade_to_rgb(FADE_TIME, 255, 255, 255)
+        else:
+            time.sleep(UPDATE_INTERVAL)
+
+# Main code
+main()
